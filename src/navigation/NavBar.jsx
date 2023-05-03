dc324d27170cf57f20591ce2fd044c251ad045f3
HaritaD <haritajee@gmail.com>
2023-05-03 13:25:01 -0500

    Added Styling to Landing Page

--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -3,25 +3,33 @@ import * as FaIcons from 'react-icons/fa';
 import './NavBar.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 const NavBar = () => {
   return (
     <div>
       <header>
-        <nav>
-          <div className="nav-menu">
-            <FaIcons.FaBars />
+        <nav class="navbar">
+          <div className="d-flex justify-content-between vw-100">
+            <div className="container d-flex justify-content-between">
+              <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <h1>ConnectedSpine</h1>
+            </div>
+            <div className="container d-flex d-flex justify-content-end">
+              <button className="btn btn-light border-0">
+                <i className="bi bi-bootstrap-reboot text-end"></i>
+              </button>
+            </div>
           </div>
-          <h1>ConnectedSpine</h1>
-          <Button size="sm" variant="secondary" className="start-over-button">
-            Start over
-          </Button>
         </nav>
       </header>
       <main>
         <Outlet />
       </main>
-    </div>
+    </div >
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,23 +7,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Home = () => {
   return (
     <div className="homepage">
-      <div className="title-text">
-        <h1>We connect you to the care you need.</h1>
-        <p>
-          ConnectedSpine uses the knowledge of spine care specialists to guide
-          you to the right providers.
-        </p>
+      <div className="container-fluid">
+        <div class="row">
+          <div class="col-md-6">
+            <div className="title-text">
+              <div className="border border-dark ">
+                <h1>We connect you to the care you need.</h1>
+              </div>
+              <p >
+                ConnectedSpine uses the knowledge of spine care specialists to guide
+                you to the right providers.
+              </p>
+            </div>
+            <div className="instructions">
+              <p className="text-muted">Experiencing pain or discomfort in your back or neck?</p>
+              <p ><b>Take our 5 min assessment.</b></p>
+
+            </div>
+            <button className="btn btn-light start-assessment-button">
+              Start Assessment
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="instructions">
-        <p>Experiencing pain or discomfort in your back or neck?</p>
-        <p>
-          <b>Take our 5 min assessment.</b>
-        </p>
-      </div>
-      <Button className="start-assessment-button" variant="dark">
-        Start Assessment
-      </Button>{' '}
-    </div>
+    </div >
   );
 };
 
